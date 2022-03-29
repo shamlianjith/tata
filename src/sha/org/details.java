2889a7b277a1739651f63dd7f74595891f2eb449
shamjithchandran <shamjithchandran005@gmail.com>
2022-03-29 14:49:55 +0530

    add project

---
+++ src/sha/org/details.java
@@ -0,0 +1,15 @@
+package sha.org;
+
+public class details {
+private void companydetails() {
+System.out.println("company name is tata");
+}
+private void employeedetails() {
+System.out.println("employee name is shamjith");
+}
+public static void main(String[] args) {
+	details d = new details();
+	d.companydetails();
+	d.employeedetails();
+}
+}
